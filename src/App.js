164c531fb5baeaa9acdf87182c0f67a28570ae4b
nafishasalsabil <nafishasalsabil16@gmail.com>
2024-02-18 23:12:14 +0600

    homepage complete

--- src/App.js
+++ src/App.js
@@ -4,26 +4,30 @@ import Navbar from './components/navbar/navbar.component';
 import Titlebar from './components/titlebar/titlebar.component';
 import Dashboard from './components/dashboard/dashboard.component';
 import Sale from './components/sale/sale.component';
+import FeaturedItems from './components/featured-items/featured-item.component';
 
 
 const App = () => {
     const [data, setData] = useState([]);
     const [cat, setCat] = useState([]);
+    const [policies, setPolicies] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await fetch('https://dummyjson.com/products');
-                const resCat = await fetch('../side-menu.json');
+                const resCat = await fetch('../jsons/side-menu.json');
+                const resPol = await fetch('../jsons/policies.json');
                 if (!response.ok) {
                     throw new Error('Failed to fetch data');
                 }
                 const jsonData = await response.json();
                 const jsonCat = await resCat.json();
+                const jsonPol = await resPol.json();
                 setData(jsonData);
                 setCat(jsonCat);
-                console.log(jsonData)
-                console.log(jsonCat);
+                setPolicies(jsonPol);
+                console.log(jsonData);
             } catch (error) {
                 console.error('Error fetching data:', error);
             }
@@ -41,6 +45,7 @@ const App = () => {
       <Navbar/>
       <Dashboard cat = {cat}/>
       <Sale/>
+      <FeaturedItems policies = {policies} data={data}/>
       </div>
       
     </div>
---
+++ src/components/featured-items/featured-item.component.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import './featured-item.styles.css';
+
+const FeaturedItems = ({ policies, data }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const productsPerPage = 4;
+
+  const handleNext = () => {
+    if (data && data.products) {
+      setActiveIndex(prevIndex => Math.min(prevIndex + 1, Math.ceil(data.products.length / productsPerPage) - 1));
+    }
+  };
+
+  const handlePrev = () => {
+    if (data && data.products) {
+      setActiveIndex(prevIndex => Math.max(prevIndex - 1, 0));
+    }
+  };
+
+
+  const startIndex = activeIndex * productsPerPage;
+  const endIndex = startIndex + productsPerPage;
+  const visibleProducts = (data && data.products) ? data.products.slice(startIndex, endIndex) : [];
+
+  return (
+    <div className="feature-container">
+      <div className="left-options">
+        {policies.map((item, index) => (
+          <div key={index} className="policy">
+            <div className="policy-icon-container">
+              <img className="policy-img" src={`../../../${item.imgUrl}`} alt="policy" />
+            </div>
+            <div className="desc-container">
+              <h3 className="pol-head">{item.heading}</h3>
+              <span className="desc-span">{item.description}</span>
+            </div>
+          </div>
+        ))}
+      </div>
+      
+      <div className="right-features">
+        <div className="feature-heading">
+          <h3>Featured Item</h3>
+          <div className="next-img">
+            <i className="arrowx left" onClick={handlePrev}></i>
+            <i className="arrowx right" onClick={handleNext}></i>
+          </div>
+        </div>
+        <div className="product-container">
+          {visibleProducts.map((item, index) => {
+            const presentPrice = item.price - (item.price * (item.discountPercentage/ 100));
+            return (
+              <div key={index} className="each-product">
+              <div className='img-cont' style={{ position: 'relative', width: '100%' }}>
+                  <span className='show-disc' style={{ position: 'absolute', top: '0', left: '0', padding: '5px', borderRadius: '5px' }}>{item.discountPercentage}%</span>
+                  <img className="prod-img" src={item.thumbnail} alt="prod" />
+                </div>
+                <div>
+                  <h3>{item.title}</h3>
+                </div>
+                <div className="rating-stock">
+                  <div className="rating">
+                    <div className={`stars rating-${Math.round(item.rating)}`}>
+                      {item.rating}
+                    </div>
+                  </div>
+                  <div className="stock" style={{ color: item.stock > 0 ? '#5FB568' : 'red' }}>
+                    {item.stock > 0 ? 'Stock available' : 'Out of stock'}
+                  </div>
+                </div>
+                <div className='price-container'>
+                  <span className='prev-price'>${item.price}</span>
+                  <span className='pres-price'>${presentPrice.toFixed(2)}</span> {/* Display present price */}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedItems;
